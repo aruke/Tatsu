c5f244cbe953ce94ddc2b9444df1a2e307d13996
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2019-02-16 10:36:36 +0900

    Update view for timer fragment

--- app/src/main/java/org/rionlabs/tatsu/ui/screen/main/timer/TimerFragment.kt
+++ app/src/main/java/org/rionlabs/tatsu/ui/screen/main/timer/TimerFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import org.rionlabs.tatsu.databinding.FragmentTimerBinding
 import org.rionlabs.tatsu.ui.screen.main.BaseMainFragment
-import timber.log.Timber
 
 class TimerFragment : BaseMainFragment() {
 
@@ -29,15 +27,8 @@ class TimerFragment : BaseMainFragment() {
         viewModel = ViewModelProviders.of(this).get(TimerViewModel::class.java)
         binding.viewModel = viewModel
 
-        viewModel.getDuration().observe(this, Observer {
-            binding.viewTimerDigits.duration = it
-        })
-
-        viewModel.timerData.observe(this, Observer {
-            it?.let { timer ->
-                Timber.d("OnChanged called with NonNull Timer value")
-                binding.textTimerStatus.text = timer.state.toString()
-            }
-        })
+        binding.buttonTimer.setOnClickListener {
+            viewModel.startTimer()
+        }
     }
 }
--- app/src/main/java/org/rionlabs/tatsu/ui/screen/main/timer/TimerViewModel.kt
+++ app/src/main/java/org/rionlabs/tatsu/ui/screen/main/timer/TimerViewModel.kt
@@ -4,13 +4,21 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import org.rionlabs.tatsu.data.model.Timer
+import org.rionlabs.tatsu.TatsuApp
+import timber.log.Timber
 
 class TimerViewModel(val app: Application) : AndroidViewModel(app) {
 
-    private val mTimerData: MutableLiveData<Timer> = MutableLiveData()
-    val timerData: LiveData<Timer>
-        get() = mTimerData
+    private val timerController = (app as TatsuApp).timerController
+
+    fun startTimer() {
+        timerController.startNewTimer(20).observeForever {
+            it?.let { timer ->
+                Timber.d("OnChanged called with NonNull Timer value")
+                Timber.d("$timer")
+            }
+        }
+    }
 
     fun getDuration(): LiveData<Long> {
         return MutableLiveData<Long>()
