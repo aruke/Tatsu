4f83a0d5bdc3e9e4a3bfc236e42b3251dc4ec6ce
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2019-02-17 23:03:39 +0900

    Connect timer to the view

--- app/src/main/java/org/rionlabs/tatsu/ui/screen/main/timer/TimerFragment.kt
+++ app/src/main/java/org/rionlabs/tatsu/ui/screen/main/timer/TimerFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import org.rionlabs.tatsu.data.model.TimerState
 import org.rionlabs.tatsu.databinding.FragmentTimerBinding
 import org.rionlabs.tatsu.ui.screen.main.BaseMainFragment
+import timber.log.Timber
 
 class TimerFragment : BaseMainFragment() {
 
@@ -26,9 +29,87 @@ class TimerFragment : BaseMainFragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(TimerViewModel::class.java)
         binding.viewModel = viewModel
+    }
 
-        binding.buttonTimer.setOnClickListener {
-            viewModel.startTimer()
-        }
+    override fun onResume() {
+        super.onResume()
+        viewModel.getTimerState().observe(this, Observer {
+            it?.let { state ->
+                binding.apply {
+                    textTimerStatus.text = state.name
+                    when (state) {
+                        TimerState.IDLE -> {
+                            buttonTimer.isEnabled = true
+                            buttonTimer.text = "Start Timer"
+                            buttonTimer.setOnClickListener {
+                                viewModel?.startNewTimer()
+                            }
+                            buttonTimer.setOnLongClickListener(null)
+                        }
+                        TimerState.RUNNING -> {
+                            buttonTimer.isEnabled = true
+                            buttonTimer.text = "Pause Timer"
+                            buttonTimer.setOnClickListener {
+                                viewModel?.pauseTimer()
+                            }
+                            buttonTimer.setOnLongClickListener {
+                                viewModel?.cancelTimer()
+                                true
+                            }
+                        }
+                        TimerState.PAUSED -> {
+                            buttonTimer.isEnabled = true
+                            buttonTimer.text = "Resume Timer"
+                            buttonTimer.setOnClickListener {
+                                viewModel?.resumeTimer()
+                            }
+                            buttonTimer.setOnLongClickListener {
+                                viewModel?.cancelTimer()
+                                true
+                            }
+                        }
+                        TimerState.STOPPED -> {
+                            buttonTimer.isEnabled = true
+                            buttonTimer.text = "Restart Timer"
+                            buttonTimer.setOnClickListener {
+                                viewModel?.startNewTimer()
+                            }
+                            buttonTimer.setOnLongClickListener(null)
+                        }
+                        TimerState.CANCELLED -> {
+                            buttonTimer.isEnabled = true
+                            buttonTimer.text = "Start Timer"
+                            buttonTimer.setOnClickListener {
+                                viewModel?.startNewTimer()
+                            }
+                            buttonTimer.setOnLongClickListener(null)
+                        }
+                    }
+                }
+            }
+        })
+
+        viewModel.getDuration().observe(this, Observer {
+            it?.let { durationInSeconds ->
+                binding.apply {
+                    Timber.d("Duration in seconds = $durationInSeconds")
+                    textTimerDivider.visibility = if (durationInSeconds % 2L == 0L) {
+                        Timber.d("Divider visibility : visible")
+                        View.VISIBLE
+                    } else {
+                        Timber.d("Divider visibility : invisible")
+                        View.INVISIBLE
+                    }
+
+                    val minutes = durationInSeconds / 60
+                    val hours = durationInSeconds / 3600
+
+                    Timber.d("Duration display = $hours:$minutes")
+
+                    textDigitMinutes.text = minutes.toString()
+                    textDigitHours.text = hours.toString()
+                }
+            }
+        })
     }
 }
--- app/src/main/java/org/rionlabs/tatsu/ui/screen/main/timer/TimerViewModel.kt
+++ app/src/main/java/org/rionlabs/tatsu/ui/screen/main/timer/TimerViewModel.kt
@@ -4,24 +4,65 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import org.rionlabs.tatsu.TatsuApp
+import org.rionlabs.tatsu.data.model.Timer
+import org.rionlabs.tatsu.data.model.TimerState
+import org.rionlabs.tatsu.ui.screen.main.settings.SettingsManager
 import timber.log.Timber
 
 class TimerViewModel(val app: Application) : AndroidViewModel(app) {
 
     private val timerController = (app as TatsuApp).timerController
 
-    fun startTimer() {
-        timerController.startNewTimer(20).observeForever {
-            it?.let { timer ->
-                Timber.d("OnChanged called with NonNull Timer value")
-                Timber.d("$timer")
-            }
+    private val settingManager = SettingsManager(app)
+
+    private val durationData = MutableLiveData<Long>()
+
+    private val stateData = MutableLiveData<TimerState>()
+
+    private val metadataObserver = Observer<Timer> {
+        it?.let { timer ->
+            Timber.d("OnChanged called with NonNull Timer value")
+            Timber.d("$timer")
+
+            durationData.value = timer.duration
+            stateData.value = timer.state
         }
     }
 
+    init {
+        if (timerController.isActiveTimerAvailable()) {
+            timerController.getActiveTimer().observeForever(metadataObserver)
+        } else {
+            durationData.value = settingManager.getWorkTimerInMinutes().toLong()
+            stateData.value = TimerState.IDLE
+        }
+    }
+
+    fun startNewTimer() {
+        val duration = settingManager.getWorkTimerInMinutes().toLong() * 60
+        timerController.startNewTimer(duration).observeForever(metadataObserver)
+    }
+
+    fun pauseTimer() {
+        timerController.pauseTimer()
+    }
+
+    fun resumeTimer() {
+        timerController.resumeTimer()
+    }
+
+    fun cancelTimer() {
+        timerController.cancelTimer()
+    }
+
     fun getDuration(): LiveData<Long> {
-        return MutableLiveData<Long>()
+        return durationData
+    }
+
+    fun getTimerState(): LiveData<TimerState> {
+        return stateData
     }
 
     override fun onCleared() {
