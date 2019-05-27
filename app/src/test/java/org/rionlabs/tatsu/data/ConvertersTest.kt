34ef84dca59eca522eaac6cf460189669e067668
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2019-05-27 23:42:43 +0900

    Added tests for converters

---
+++ app/src/test/java/org/rionlabs/tatsu/data/ConvertersTest.kt
@@ -0,0 +1,39 @@
+package org.rionlabs.tatsu.data
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.rionlabs.tatsu.data.model.TimerState
+import org.rionlabs.tatsu.data.model.TimerType
+
+class ConvertersTest {
+
+    @Test
+    fun `test TimerState to String`() {
+        assertEquals("IDLE", Converters.toString(TimerState.IDLE))
+        assertEquals("RUNNING", Converters.toString(TimerState.RUNNING))
+        assertEquals("PAUSED", Converters.toString(TimerState.PAUSED))
+        assertEquals("STOPPED", Converters.toString(TimerState.STOPPED))
+        assertEquals("CANCELLED", Converters.toString(TimerState.CANCELLED))
+    }
+
+    @Test
+    fun `test string to TimerState`() {
+        assertEquals(TimerState.IDLE, Converters.toTimerState("IDLE"))
+        assertEquals(TimerState.RUNNING, Converters.toTimerState("RUNNING"))
+        assertEquals(TimerState.PAUSED, Converters.toTimerState("PAUSED"))
+        assertEquals(TimerState.STOPPED, Converters.toTimerState("STOPPED"))
+        assertEquals(TimerState.CANCELLED, Converters.toTimerState("CANCELLED"))
+    }
+
+    @Test
+    fun `test TimerType to String`() {
+        assertEquals("WORK", Converters.toString(TimerType.WORK))
+        assertEquals("BREAK", Converters.toString(TimerType.BREAK))
+    }
+
+    @Test
+    fun `test String to TimerType`() {
+        assertEquals(TimerType.WORK, Converters.toTimerType("WORK"))
+        assertEquals(TimerType.BREAK, Converters.toTimerType("BREAK"))
+    }
+}
