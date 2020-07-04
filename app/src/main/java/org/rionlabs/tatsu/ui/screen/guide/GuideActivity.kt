9539f3227776e039202337b39b69c6a6e176019f
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-07-04 14:08:15 +0900

    Added data in introduction guide

--- app/src/main/java/org/rionlabs/tatsu/ui/screen/guide/GuideActivity.kt
+++ app/src/main/java/org/rionlabs/tatsu/ui/screen/guide/GuideActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.github.paolorotolo.appintro.AppIntro2
-import com.github.paolorotolo.appintro.AppIntroFragment
+import com.github.appintro.AppIntro2
+import com.github.appintro.AppIntroFragment
+import com.github.appintro.AppIntroPageTransformerType
+import com.github.appintro.model.SliderPage
 import org.rionlabs.tatsu.R
 import org.rionlabs.tatsu.ui.screen.main.MainActivity
 import org.rionlabs.tatsu.work.PreferenceManager
@@ -15,38 +17,51 @@ class GuideActivity : AppIntro2() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        showSkipButton(false)
+        isSkipButtonEnabled = false
+
+        val defaultSliderPage = SliderPage(
+            backgroundColor = ContextCompat.getColor(this, R.color.colorPrimary),
+            titleColor = ContextCompat.getColor(this, R.color.textColorLightPrimary),
+            descriptionColor = ContextCompat.getColor(this, R.color.textColorLightSecondary),
+            titleTypefaceFontRes = R.font.raleway,
+            descriptionTypefaceFontRes = R.font.raleway
+        )
 
         addSlide(
             AppIntroFragment.newInstance(
-                "Guide Title 1", null,
-                "Guide Description 1", null,
-                R.mipmap.ic_launcher_round,
-                ContextCompat.getColor(this, R.color.colorPrimary),
-                ContextCompat.getColor(this, R.color.textColorLightPrimary),
-                ContextCompat.getColor(this, R.color.textColorLightSecondary)
+                defaultSliderPage.copy(
+                    title = "Simple Pomodoro Timer",
+                    description = "Manage your with simple timer. Set work repetitions, Take short breaks!",
+                    imageDrawable = R.drawable.undraw_time_management
+                )
             )
         )
 
         addSlide(
             AppIntroFragment.newInstance(
-                "Guide Title 2", null,
-                "Guide Description 2", null,
-                R.mipmap.ic_launcher_round,
-                ContextCompat.getColor(this, R.color.colorPrimary),
-                ContextCompat.getColor(this, R.color.textColorLightPrimary),
-                ContextCompat.getColor(this, R.color.textColorLightSecondary)
+                defaultSliderPage.copy(
+                    title = "Keep track of Productivity",
+                    description = "Track your productive time & break times with simple statistics!",
+                    imageDrawable = R.drawable.undraw_dev_productivity
+                )
             )
         )
 
         addSlide(
             AppIntroFragment.newInstance(
-                "Guide Title 3", null,
-                "Guide Description 3", null,
-                R.mipmap.ic_launcher_round,
-                ContextCompat.getColor(this, R.color.colorPrimary),
-                ContextCompat.getColor(this, R.color.textColorLightPrimary),
-                ContextCompat.getColor(this, R.color.textColorLightSecondary)
+                defaultSliderPage.copy(
+                    title = "Keep the Distractions Away",
+                    description = "Turn on Silent Mode to mute all notifications when you are working!",
+                    imageDrawable = R.drawable.undraw_project_completed
+                )
+            )
+        )
+
+        setTransformer(
+            AppIntroPageTransformerType.Parallax(
+                titleParallaxFactor = -1.0,
+                imageParallaxFactor = 100.0,
+                descriptionParallaxFactor = -1.0
             )
         )
     }
