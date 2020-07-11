10400097d3f9278f0b66d7e2ab1ca7d8ee9f8c2b
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-07-12 05:56:54 +0900

    Fix digital timer

--- app/src/main/java/org/rionlabs/tatsu/ui/view/DigitalTimerView.kt
+++ app/src/main/java/org/rionlabs/tatsu/ui/view/DigitalTimerView.kt
@@ -3,10 +3,12 @@ package org.rionlabs.tatsu.ui.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import org.rionlabs.tatsu.data.model.Timer
 import org.rionlabs.tatsu.databinding.ViewDigitalTimerBinding
 import timber.log.Timber
+import java.util.*
 
 class DigitalTimerView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -14,24 +16,35 @@ class DigitalTimerView @JvmOverloads constructor(
 
     private val binding = ViewDigitalTimerBinding.inflate(LayoutInflater.from(context), this, true)
 
+    private val zeroPaddedFormat: String = "%02d"
+    private val normalFormat: String = "%d"
+
     fun setTimer(timer: Timer) {
         binding.apply {
             Timber.d("Duration in seconds = $timer")
             timer.apply {
                 Timber.d("Duration display = $hours:$minutes:$seconds")
+                var minuteFormat = zeroPaddedFormat
+                var secondFormat = zeroPaddedFormat
 
                 if (hours <= 0) {
                     groupHour.visibility = GONE
+                    minuteFormat = normalFormat
+                } else {
+                    groupHour.visibility = View.VISIBLE
                 }
                 textDigitHours.text = hours.toString()
 
                 if (minutes <= 0) {
                     groupMinutes.visibility = GONE
+                    secondFormat = normalFormat
+                } else {
+                    groupMinutes.visibility = View.VISIBLE
                 }
 
-                textDigitMinutes.text = minutes.toString()
+                textDigitMinutes.text = String.format(Locale.US, minuteFormat, minutes)
 
-                textDigitSeconds.text = seconds.toString()
+                textDigitSeconds.text = String.format(Locale.US, secondFormat, seconds)
             }
         }
         binding.executePendingBindings()
