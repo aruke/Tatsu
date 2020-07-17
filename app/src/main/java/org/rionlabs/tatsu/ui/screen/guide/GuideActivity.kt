f8a278bf85ba05d579e4ba3345bd2ca63d5b3158
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-07-17 22:04:21 +0900

    Updated Guide slides with Lotties

--- app/src/main/java/org/rionlabs/tatsu/ui/screen/guide/GuideActivity.kt
+++ app/src/main/java/org/rionlabs/tatsu/ui/screen/guide/GuideActivity.kt
@@ -2,13 +2,10 @@ package org.rionlabs.tatsu.ui.screen.guide
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.ColorUtils
 import androidx.fragment.app.Fragment
 import com.github.appintro.AppIntro2
-import com.github.appintro.AppIntroFragment
+import com.github.appintro.AppIntroCustomLayoutFragment
 import com.github.appintro.AppIntroPageTransformerType
-import com.github.appintro.model.SliderPage
 import org.koin.android.ext.android.get
 import org.rionlabs.tatsu.R
 import org.rionlabs.tatsu.ui.screen.main.MainActivity
@@ -21,42 +18,9 @@ class GuideActivity : AppIntro2() {
 
         isSkipButtonEnabled = false
 
-        val defaultSliderPage = SliderPage(
-            backgroundColor = ContextCompat.getColor(this, R.color.color_primary),
-            descriptionColor = ColorUtils.setAlphaComponent(
-                ContextCompat.getColor(this, R.color.color_on_primary), 100
-            )
-        )
-
-        addSlide(
-            AppIntroFragment.newInstance(
-                defaultSliderPage.copy(
-                    title = getString(R.string.guide_title_1),
-                    description = getString(R.string.guide_description_1),
-                    imageDrawable = R.drawable.undraw_time_management
-                )
-            )
-        )
-
-        addSlide(
-            AppIntroFragment.newInstance(
-                defaultSliderPage.copy(
-                    title = getString(R.string.guide_title_2),
-                    description = getString(R.string.guide_description_2),
-                    imageDrawable = R.drawable.undraw_dev_productivity
-                )
-            )
-        )
-
-        addSlide(
-            AppIntroFragment.newInstance(
-                defaultSliderPage.copy(
-                    title = getString(R.string.guide_title_3),
-                    description = getString(R.string.guide_description_3),
-                    imageDrawable = R.drawable.undraw_project_completed
-                )
-            )
-        )
+        addSlide(AppIntroCustomLayoutFragment.newInstance(R.layout.slide_guide_1))
+        addSlide(AppIntroCustomLayoutFragment.newInstance(R.layout.slide_guide_2))
+        addSlide(AppIntroCustomLayoutFragment.newInstance(R.layout.slide_guide_3))
 
         setTransformer(
             AppIntroPageTransformerType.Parallax(
